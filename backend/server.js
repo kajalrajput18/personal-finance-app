a8128a526ca9950797151e5baf11f3b6cd4a7bbd
Kajal <kajalrajput9315@gmail.com>
2026-01-16 23:32:31 +0530

    backend setup

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,24 @@
-require("dotenv").config();
 const express = require("express");
+const dotenv = require("dotenv");
 const cors = require("cors");
-const path = require("path");
+
+const connectDB = require("./configs/db");
+
+dotenv.config(); 
+
+connectDB(); 
+
 const app = express();
-app.use(
-    cors({
-        origin: process.env.CLIENT_URL || "*",
-        methods: ["GET", "POST", "PUT", "DELETE"],
-        allowedHeaders: ["Content-Type", "Authorization"],
-    })
-);
+
+app.use(cors());
 app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Personal Finance Backend is running 🚀");
+});
+
 const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`🚀 Server running on port ${PORT}`);
 });
