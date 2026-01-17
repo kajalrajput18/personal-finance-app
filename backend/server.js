cc93c8eb81bf6d19ec9cfa4e7946e06ac7d8da75
Kajal <kajalrajput9315@gmail.com>
2026-01-17 19:16:09 +0530

    Implemented User Auth

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,9 @@ const cors = require("cors");
 
 const connectDB = require("./configs/db");
 
+const authRoutes = require("./routes/authRoutes");
+const { protect } = require("./middleware/authMiddleware");
+
 dotenv.config(); 
 
 connectDB(); 
@@ -12,11 +15,17 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use("/api/auth", authRoutes);
 
 app.get("/", (req, res) => {
   res.send("Personal Finance Backend is running 🚀");
 });
-
+app.get("/api/protected", protect, (req, res) => {
+  res.json({
+    message: "Access granted",
+    user: req.user
+  });
+});
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
